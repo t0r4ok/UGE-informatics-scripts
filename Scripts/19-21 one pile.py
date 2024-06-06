4fc9563b6e0e45d0e3fd75ba05fd37d764a0ac73
Tor4ok <tima.sozin@gmail.com>
2024-06-06 12:04:03 +0300

    Update 19-21 one pile.py

--- Scripts/19-21 one pile.py
+++ Scripts/19-21 one pile.py
@@ -34,10 +34,9 @@ def f(stones, turn):
         f(stones * 3, turn - 1)
     ]
 
-    return any(actions) if (turn - 1) == 0 else all(actions)
+    return any(actions) if (turn - 1) % 2 == 0 else all(actions)
 
 
-# первый ход делает Петя
 print('19)', [s for s in range(1, 435) if f(s, 2)])
 print('20)', [s for s in range(1, 435) if not f(s, 1) and f(s, 3)])
 print('21)', [s for s in range(1, 435) if not f(s, 2) and f(s, 4)])
